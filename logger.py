ba43eeb23a91fa9e9f7ebf143c3bc2a39d7bd9b4
HugoBar <hugomdbarreiro@gmail.com>
2026-01-07 15:05:17 +0000

    Add colors to logger

--- logger.py
+++ logger.py
@@ -1,7 +1,28 @@
 import logging
 
-# Configure logging globally
-logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
+
+class ColoredFormatter(logging.Formatter):
+    COLORS = {
+        "DEBUG": "\033[94m",  # Blue
+        "INFO": "\033[92m",  # Green
+        "WARNING": "\033[93m",  # Yellow
+        "ERROR": "\033[91m",  # Red
+        "CRITICAL": "\033[95m",  # Magenta
+    }
+    RESET = "\033[0m"
+
+    def format(self, record):
+        color = self.COLORS.get(record.levelname, self.RESET)
+        record.levelname = f"{color}{record.levelname}{self.RESET}"
+        record.msg = f"{color}{record.msg}{self.RESET}"
+        return super().format(record)
+
+
+# Configure logging globally with colors
+handler = logging.StreamHandler()
+handler.setFormatter(ColoredFormatter("%(levelname)s: %(message)s"))
+logging.getLogger().addHandler(handler)
+logging.getLogger().setLevel(logging.INFO)
 
 # Create a global logger instance
 logger = logging.getLogger("vidfx")
